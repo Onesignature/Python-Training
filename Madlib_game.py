2e3cf3eea6b4a34f6f26479a801024893fd1e39a
Bilal Saeed <bilalsaeedahmed1@gmail.com>
2024-09-24 00:46:36 +0400

    User input fill in the blanks basic

---
+++ Madlib_game.py
@@ -0,0 +1,5 @@
+adjective1 = input("Enter an adjective (Description of something): ")
+noun = input("Enter a noun(Place person or thing): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun}")
